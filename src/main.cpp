c17dd7424165d5bcb65a19446a487538b269c316
zrobertson <zachary_w_robertson>
2017-09-06 16:59:15 -0500

    broken first attempt

--- src/main.cpp
+++ src/main.cpp
@@ -5,12 +5,115 @@
 #include "argagg.hpp"
 
 using namespace std::chrono;
+using argagg::parser_results;
+using argagg::parser;
+using std::cerr;
+using std::cout;
+using std::endl;
+using std::ofstream;
+using std::ostream;
+using std::ostringstream;
+using std::string;
 
 int main(int argc, char *argv[])
 {
 
     MatrixSimulator simulator;
 //    create_fits_file("../simulations/mdwarf_13bil_0m.fit");
+
+    parser argparser {{
+
+                              {
+                                      "help", {"-h", "--help"},
+                                      "Print help and exit", 0
+                              },
+
+                              {
+                                      "blackbody", {"-b", "--blackbody"},
+                                      "Simulate a blackbody with effective temperature K and magnitude M", 0
+                              },
+
+                              {
+
+                                      "temperature", {"-T", "--temperature"},
+                                      "Input temperature for source", 1
+
+                              },
+
+                              {
+
+                                      "magnitude", {"-m", "--magnitude"},
+                                      "Input magnitude for source", 1
+
+                              },
+
+                      }};
+
+    // Define our usage text. (Really poor quality)
+    ostringstream usage;
+    usage
+            << argv[0] << " 1.0" << endl
+            << endl
+            << "Usage: " << argv[0] << " [OPTIONS]... [FILES]..." << endl
+            << endl;
+
+    // Use our argument parser to... parse the command line arguments. If there
+    // are any problems then just spit out the usage and help text and exit.
+    argagg::parser_results args;
+
+    try {
+        args = argparser.parse(argc, argv);
+    } catch (const std::exception& e) {
+        argagg::fmt_ostream fmt(cerr);
+        fmt << usage.str() << argparser << endl
+            << "Encountered exception while parsing arguments: " << e.what()
+            << endl;
+        return EXIT_FAILURE;
+    }
+
+    // If the help flag was specified then spit out the usage and help text and
+    // exit.
+    if (args["help"]) {
+        argagg::fmt_ostream fmt(cerr);
+        fmt << usage.str() << argparser;
+        return EXIT_SUCCESS;
+    }
+
+    double temp;
+    double mag;
+
+    if (args["temperature"]) {
+
+        temp = args["temperature"].as<double>();
+
+    }
+    else{
+        temp = 6500;
+    }
+
+    if (args["magnitude"]) {
+
+        cout<<"Stae";
+        mag = args["magnitude"].as<double>();
+
+    }
+    else{
+        mag = 0;
+    }
+
+    if (args["blackbody"]) {
+
+        cout<<"Stae";
+        Blackbody cs = Blackbody(temp,mag);
+
+    }
+    else{
+
+        Constant cs = Constant(10E-8);
+
+    }
+
+
 
     for (int i=1; i<2; ++i){
         high_resolution_clock::time_point t1 = high_resolution_clock::now();
@@ -26,8 +129,8 @@ int main(int argc, char *argv[])
 
 //        Blackbody cs = Blackbody(9602., 0.);
 
+          Constant cs = Constant(10E-8);
 
-        Constant cs = Constant(10E-8);
 //          PhoenixSpectrum cs = PhoenixSpectrum("/data/CppLibs/7125_0_lte03200-5.50-0.0.PHOENIX-ACES-AGSS-COND-2011-HiRes_2.fits",
 //          "/data/CppLibs/WAVE_PHOENIX-ACES-AGSS-COND-2011.fits",0.45, 0.85, 0.);
 //        IdealEtalon cs = IdealEtalon(5., 1., 0., 0.95);
